14caf647192d726dc16813dd9122a951588cd7e9
DanielAlvarezCastro <dalvar06@ucm.es>
2019-04-29 15:48:19 +0200

    JsActual

---
+++ modelo.js
@@ -0,0 +1,237 @@
+//Informacion de la partida
+actualRol_= "VAMPIRE";
+lastRol_="";
+numPlayers__= 8;
+players_= [];
+players_[0] = "VAMPIRE";
+players_[1] = "HUNTER";
+players_[2] = "FARMER";
+players_[3] = "FARMER";
+players_[4] = "FARMER";
+players_[5] = "VAMPIRE";
+players_[6] = "FARMER";
+players_[7] = "FARMER";
+vampireAmount = 2;
+vampireVotes = [];
+farmVotes= [];
+votes = [];
+
+function statusUpdate() {
+	this.id = '';
+	this.deaths = [];
+	this.logs = [];
+	this.newRol ='';
+}
+/*Para consultar
+playJSON = {
+    rol: 'VAMPIRE',
+    client: clientPlayer,
+    victim: victim_,
+}
+*/
+
+function recivePlay(playJSON)//Tambien recibirá el estado de la partida
+{
+	//Se hactualiza la infromación de la partida desde la BD
+
+	//Get the player, rol, and action of the play and process it
+	var play = JSON.parse(playJSON);
+	object = new statusUpdate();
+	switch(play.rol)
+	{
+		case 'VAMPIRE':
+			vampireMove(play, object); //Se le envía el nuevo estado al servidor
+			return JSON.stringify(object);
+			break;
+		case 'HUNTER':
+			hunterMove(play, object);
+			return JSON.stringify(object);
+			break;
+		case 'POPULAR_VOTE':
+			popularMove(play, object);
+			return JSON.stringify(object);
+			break;
+	}
+}
+
+function popularMove(play, object)
+{
+	farmVotes.push(play.victim);
+	if(farmVotes.length == countMaxVotes)
+	{
+		i =evenRepeatVotationCount();
+		if(i<0)
+		{
+			//Repite Votacion
+			object.logs.push("Popular voting tied! Decide again!");
+			object.id = 'POPULAR_EVEN';
+		}
+		else
+		{
+			object.deaths.push(i);
+			object.id = 'POPULAR_VOTED';
+			startNight(object);//Termina la noche (provisional, esto lo acabará haciendo la bruja)
+		}
+	}
+	else {
+		object.id = 'CONTINUE_VOTATION';
+		object.logs.push("Player " + (play.clientPlayer +1) +" voted Player " + (play.victim+1)+"!");
+	}
+}
+
+function hunterMove(play, object)
+{
+	object.deaths.push(play.victim_);
+	object.id  = 'HUNTER_SHOT';
+	object.logs.push("Player "+ (play.clientPlayer+1) +" has shot Player "+ (play.victim+1) +"!")
+	object.logs.push("Player "+ (play.clientPlayer+1) +" has died!");
+	//El cazador muere
+	players_[object.clientPlayer] == "DEAD";
+	processDeaths(object);
+	if(lastRol_ == "POPULAR_VOTATION"){//Si le ha matado el pueblo, empieza la noche
+		startNight(object);
+	}
+	else{//Si le han matado los vampiros, termina la noche
+		lastRol_="HUNTER";
+		actualRol_="POPULAR_VOTATION";
+		object.newRol = actualRol_;
+	}
+
+}
+
+function vampireMove(play, object)
+{
+	vampireVotes.push(play.victim);
+	if(vampireVotes.length == vampireAmount)
+	{
+		i =evenRepeatVotationCount();
+		if(i<0)
+		{
+			//Repite Votacion
+			object.logs.push("Vampiric voting tied! Hunt again!");
+			object.id = 'VAMPIRE_EVEN';
+		}
+		else
+		{
+			object.deaths.push(i);
+			object.id = 'VAMPIRES_VOTED';
+			endNight(object);//Termina la noche (provisional, esto lo acabará haciendo la bruja)
+		}
+	}
+	else {object.id = 'CONTINUE_VOTATION'}
+}
+
+function evenRepeatVotationCount()
+{
+	greatest =-1;
+    greatestVotes =-1;
+    greatestEven = -1;
+    resetVotes();
+    for(i =0; i<vampireVotes.length; i++)
+    {
+    	votes[vampireVotes[i]]+=1;
+    }
+    for(i =0; i<votes.length; i++)
+    {
+        if(votes[i]>greatestVotes)
+        {
+            greatest = i;
+            greatestVotes = votes[i];
+        }
+        else if(votes[i] == greatestVotes)
+        {
+        	greatestEven = greatestVotes;
+        }
+    }
+    votes = [];
+    if(greatestVotes == greatestEven)
+    {
+    	return -1;//Empate al mejor, se repite votacion
+    }
+    return greatest;//Mayoría
+}
+
+
+function endNight(object)
+{
+	lastRol_ = "VAMPIRE" //Provisional, será la bruja
+	actualRol_ = "POPULAR_VOTATION";
+	object.newRol = actualRol_;
+	object.logs.push("The farmers wake up");
+	processDeaths(object);
+	checkWin(object);
+	
+
+}
+
+function startNight(object)
+{
+	object.logs.push("The farmers go to bed...");
+	lastRol_ = actualRol_;
+	actualRol_ = "VAMPIRE";//Provisional, será el estrcito primer rol
+	object.newRol = actualRol_;
+}
+
+function processDeaths(object)
+{
+	for(i=0; i < object.deaths.length; i++){
+		//Se recorren los jugadores que han muerto esa noche
+		if(players_[object.deaths[i]] == "HUNTER" ){//Si el cazador muere, será su turno
+			object.logs.push("Player " +(object.deaths[i]+1) + "was the Hunter, and wants retribution!");
+			actualRol_ = "HUNTER";
+			object.newRol = actualRol_;
+			object.deaths.splice(i,1);
+		}
+		else{
+			object.logs.push("Player "+ (object.deaths[i]+1) +" has died!");
+			//El jugador muere
+			players_[object.deaths[i] == "DEAD"];
+		}
+	}
+}
+
+function checkWin(object)//Comprueba si un bando ha ganado
+{
+	vampiresLeft = 0;
+	farmersLeft = 0;
+	for(i=0; i<players_.length; i++){
+		if(players_[i] == "VAMPIRE"){vampiresLeft++;}
+		else if(players_[i] != "DEAD"){farmersLeft++;}
+	}
+	if(vampiresLeft == 0){object.id = "FARMERS_WON";}
+	else if(farmersLeft == 0){object.id = "VAMPIRES_WON";}
+}
+
+function countVotes()
+{
+    greatest =0;
+    greatestVotes =0;
+    for(i =0; i<votes.length; i++)
+    {
+        if(votes[i]>greatestVotes)
+        {
+            greatest = i;
+            greatestVotes = votes[i];
+        }
+    }
+    votes = [];
+    return greatest;
+}
+
+function countMaxVotes()
+{
+	people =0;
+	for(i=0; i<players_.length; i++){
+		if(players_[i] != "DEAD"){
+			people++;
+		}
+	}
+}
+
+function resetVotes()
+{
+    for(i =0; i < numPlayers__; i++)
+    {
+        votes[i]=0;
+    }
+}
---
+++ partida.js
@@ -0,0 +1,203 @@
+actualRol= "VAMPIRE";
+            actualTurn =-1;
+            numPlayers = 8;
+            players= [[]];
+            for(i=0; i<numPlayers; i++)
+            {
+                players[i]=[];//player rol
+                players[i][1]=0;//the player has played flag FOR DEBUG
+            }
+            players[0][0] = "VAMPIRE";
+            players[1][0] = "HUNTER";
+            players[2][0] = "FARMER";
+            players[3][0] = "FARMER";
+            players[4][0] = "FARMER";
+            players[5][0] = "VAMPIRE";
+            players[6][0] = "FARMER";
+            players[7][0] = "FARMER";
+
+            clientPlayer = 0;
+            clientRol = players[clientPlayer];
+            //nextRound();
+
+            
+            function vote(player)
+            {
+                if((actualRol == players[clientPlayer][0] || actualRol == "POPULAR_VOTATION") && players[clientPlayer][0] != "DEAD"){
+                    if(players[clientPlayer][1] == 0){
+                        
+                        switch(actualRol)
+                        {
+                            case "VAMPIRE":
+                                vampirePlay(player);
+                                break;
+                            case "HUNTER":
+                                hunterPlay(player);
+                                break;
+                            case "POPULAR_VOTATION":
+                                popularPlay(player);
+                                break;
+                        }
+                    }
+                }
+            }
+
+            function popularPlay(victim_)
+            {
+                players[clientPlayer][1] = 1;
+                //Envía jugada al servidor vía Ajax
+                playJSON = {
+                    rol: 'POPULAR_VOTE',
+                    client: clientPlayer,
+                    victim: victim_,
+                }
+                reciveStatus(recivePlay(JSON.stringify(playJSON)));//provisional
+            }
+
+            function vampirePlay(victim_)
+            {
+                players[clientPlayer][1] = 1;
+                logEntry("You pray is Player "+ (victim_+1));
+                //Envía jugada al servidor vía Ajax
+                playJSON = {
+                    rol: 'VAMPIRE',
+                    client: clientPlayer,
+                    victim: victim_,
+                }
+                reciveStatus(recivePlay(JSON.stringify(playJSON)));//provisional
+
+            }
+
+            function hunterPlay(victim_)
+            {
+                players[clientPlayer][1] = 1;
+                //Envía jugada al servidor vía Ajax
+                playJSON = {
+                    rol: 'HUNTER',
+                    client: clientPlayer,
+                    victim: victim_,
+                }
+                reciveStatus(recivePlay(JSON.stringify(playJSON)));//provisional
+
+            }
+
+
+            /*function statusUpdate() {//Para consultar
+                this.id = '';
+                this.deaths = [];
+                this.logs = [];
+                this.newRol ='';
+            }*/
+
+            function reciveStatus(newStateJSON)//Actualiza el estado del cliente via websocket
+            {
+                var newState = JSON.parse(newStateJSON);
+                switch(newState.id)
+                {
+                    case "VAMPIRES_EVEN":
+                        repeatPlay("VAMPIRE");
+                        break;
+                    case "VAMPIRES_VOTED":
+                        actualRol = newState.newRol;
+                        resetPlay();
+                        break;
+                    case "POPULAR_EVEN":
+                        repeatPlay("VAMPIRE");
+                        break;
+                    case "POPULAR_VOTED":
+                        actualRol = newState.newRol;
+                        resetPlay();
+                        break;
+                    case "HUNTER_SHOT":
+                        actualRol = newState.newRol;
+                        resetPlay();
+                        break;
+                }
+                if(newState.deaths.length >0) updateDeaths(newState.deaths);
+                printLogs(newState.logs);
+            }
+
+            function repeatPlay(rol)
+            {
+                for (var i = 0; i < players.length; i++) {
+                    if(players[i][0] == rol){
+                        players[i][1] == 0;
+                    }
+                }
+            }
+
+            function resetPlay()
+            {
+                for (var i = 0; i < players.length; i++) {
+                    players[i][1] == 0;
+                }
+            }
+
+            function updateDeaths(deaths)
+            {
+                for(i=0; i < deaths.length; i++){
+                    players[deaths[i]][0] = "DEAD";
+                    document.getElementById('player'+(deaths[i]+1)+'card').innerHTML = "DEAD";
+                }
+            }
+
+            function logEntry(message)
+            {
+                date= new Date();
+                document.getElementById('log').innerHTML += "\n"+ date.getHours()+":"
+                                                                + date.getMinutes()+":"
+                                                                + date.getSeconds()+"  "
+                                                                +message;
+                document.getElementById('note').innerHTML = message;
+            }
+
+            function printLogs(logs)
+            {
+                for(i =0; i< logs.length; i++){
+                    logEntry(logs[i]);
+                }
+            }
+
+            document.getElementById("card1").addEventListener("click", function() 
+            { vote(0);})
+            document.getElementById("card2").addEventListener("click", function() 
+            { vote(1);})
+            document.getElementById("card3").addEventListener("click", function() 
+            { vote(2);})
+            document.getElementById("card4").addEventListener("click", function() 
+            { vote(3);})
+            document.getElementById("card5").addEventListener("click", function() 
+            { vote(4);})
+            document.getElementById("card6").addEventListener("click", function() 
+            { vote(5);})
+            document.getElementById("card7").addEventListener("click", function() 
+            { vote(6);})
+            document.getElementById("card8").addEventListener("click", function() 
+            { vote(7);})
+
+            //FOR DEBUG
+            document.getElementById("player1").addEventListener("click", function() 
+            { changePlayer(0);})
+            document.getElementById("player2").addEventListener("click", function() 
+            { changePlayer(1);})
+            document.getElementById("player3").addEventListener("click", function() 
+            { changePlayer(2);})
+            document.getElementById("player4").addEventListener("click", function() 
+            { changePlayer(3);})
+            document.getElementById("player5").addEventListener("click", function() 
+            { changePlayer(4);})
+            document.getElementById("player6").addEventListener("click", function() 
+            { changePlayer(5);})
+            document.getElementById("player7").addEventListener("click", function() 
+            { changePlayer(6);})
+            document.getElementById("player8").addEventListener("click", function() 
+            { changePlayer(7);})
+
+            function changePlayer(player)
+            {
+                clientPlayer = player;
+                clientRol = players[clientPlayer][0];
+                document.getElementById('plaNo').innerHTML = "You are Player " +(player+1);
+                document.getElementById('plaRol').innerHTML = clientRol;
+
+            }
